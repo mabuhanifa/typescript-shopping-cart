e00f6e7fd8694d8cbf87d6e5ba85d26b15cf5251
shourovahmd <moh.abuhanifa@gmail.com>
2022-11-19 13:59:04 +0600

    button

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,31 @@
-import { Container, Nav, Navbar as NavbarBs } from "react-bootstrap"
+import { Button, Container, Nav, Navbar as NavbarBs } from "react-bootstrap"
 import { NavLink } from 'react-router-dom'
 export function Navbar() {
     return (
         <NavbarBs className="bg-white mb-3 shadow-sm">
             <Container>
-                <Nav>
+                <Nav className="me-auto">
                     <Nav.Link to='/' as={NavLink}>Home</Nav.Link>
                     <Nav.Link to='/store' as={NavLink}>Store</Nav.Link>
                     <Nav.Link to='/about' as={NavLink}>About</Nav.Link>
                 </Nav>
+                <Button style={{ height: "3rem", width: "3rem", position: "relative" }} variant="outline-secondary">
+                    <div
+                        className="rounded-circle bg-danger d-flex justify-content-center align-items-center"
+                        style={{
+                            color: "white",
+                            width: "1.5rem",
+                            height: "1.5rem",
+                            position: "absolute",
+                            bottom: 0,
+                            right: 0,
+                            transform: "translate(25%, 25%)",
+                        }}
+                    >
+                        3
+                    </div>
+                    <img src="https://www.svgrepo.com/show/316543/cart-1.svg" alt="" />
+                </Button>
             </Container>
         </NavbarBs>
     )

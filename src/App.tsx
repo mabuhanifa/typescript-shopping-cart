46b37a54f43afc677385cb9845c79292d5504d10
shourovahmd <moh.abuhanifa@gmail.com>
2022-11-19 13:30:03 +0600

    nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
 import { Container } from "react-bootstrap"
 import { Route, Routes } from 'react-router-dom'
+import { Nav } from "./components/Nav"
+import { About } from "./pages/About"
 import { Home } from "./pages/Home"
+import { Store } from "./pages/Store"
 
 function App() {
   return (
-    <Container>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </Container>
+    <>
+      <Nav />
+      <Container className="mb-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/store" element={<Store />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Container>
+    </>
   )
 }
 
